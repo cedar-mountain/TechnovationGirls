b06f98fed82b6dabf3dd056a6a2dee0469aaaed8
cedar-mountain <shanshanhuihu@icloud.com>
2021-04-04 00:14:07 +0900

    Initial Commit

---
+++ TechnovationGirls13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnovationGirls13
+//
+//  Created by 杉山恵呼 on 2021/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
